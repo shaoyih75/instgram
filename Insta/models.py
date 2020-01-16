6d3f9b41f534216ac635ab8ff9ac2f6de5d759fb
Shaoyi Hu <shaoyi.hu@airbnb.com>
2020-01-15 23:44:10 -0800

    add more fancy stuff

--- Insta/models.py
+++ Insta/models.py
@@ -6,19 +6,6 @@ from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class Post(models.Model):
-    title = models.TextField(blank=True, null=True)
-    image = ProcessedImageField(
-        upload_to = 'static/images/posts',
-        format = 'JPEG',
-        options = {'quality':100},
-        blank = True,
-        null = True
-    )
-
-    def get_absolute_url(self):
-        return reverse("post_detail", args=[str(self.id)])
-
 class InstaUser(AbstractUser):
     profile_pic = ProcessedImageField(
         upload_to = 'static/images/profiles',
@@ -32,3 +19,41 @@ class InstaUser(AbstractUser):
 
     def __str__(self):
             return self.username
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='my_posts'
+    )
+    title = models.TextField(blank=True, null=True)
+    image = ProcessedImageField(
+        upload_to = 'static/images/posts',
+        format = 'JPEG',
+        options = {'quality':100},
+        blank = True,
+        null = True
+    )
+
+    def get_like_count(self):
+        return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes')
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+        )
+    
+    class Meta:
+        unique_together = ("post", "user")
+    
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' likes ' + self.post.title
